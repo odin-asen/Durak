7077b14c234cd0b1363a0cbd28e2398561032000
odin-asen <geroge.forman@freenet.de>
2014-01-07 00:48:48 +0100

    Das Serverpanel wird angezeigt, wenn ein Menüeintrag gedrückt wird.

--- src/com/github/odinasen/gui/Controller.java
+++ src/com/github/odinasen/gui/Controller.java
@@ -1,5 +1,25 @@
 package com.github.odinasen.gui;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.SplitPane;
 
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Controller {
+  @FXML private SplitPane mainSplitPane;
+
+  /** Fuegt das ServerPanel zur Hauptoberflaeche hinzu. */
+  public void openServerPanel() {
+    try {
+      Parent serverPanel = FXMLLoader.load(getClass().getResource("server/server.fxml"),
+          ResourceBundle.getBundle(DurakApplication.BUNDLE_NAME, Locale.getDefault()));
+      mainSplitPane.getItems().add(0, serverPanel);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
--- src/com/github/odinasen/gui/DurakApplication.java
+++ src/com/github/odinasen/gui/DurakApplication.java
@@ -13,7 +13,7 @@ import java.util.ResourceBundle;
 
 public class DurakApplication extends Application implements Observer {
 
-  private static final String BUNDLE_NAME = "com.github.odinasen.i18n.gui";
+  public static final String BUNDLE_NAME = "com.github.odinasen.i18n.gui";
   private static final String TITLE = "Durak";
   private static final String MAIN_FXML = "main_content.fxml";
 
@@ -30,8 +30,7 @@ public class DurakApplication extends Application implements Observer {
   @Override
   public void start(Stage primaryStage) throws Exception {
     ResourceBundle bundle = ResourceBundle.getBundle(BUNDLE_NAME, Locale.getDefault());
-    Parent root = FXMLLoader.load(
-        getClass().getResource(MAIN_FXML), bundle);
+    Parent root = FXMLLoader.load(getClass().getResource(MAIN_FXML), bundle);
     primaryStage.setTitle(TITLE);
     primaryStage.setScene(new Scene(root, 300, 275));
     primaryStage.show();
