1d396144cdc317743e4e28998162778edf8d09b5
odin-asen <geroge.forman@freenet.de>
2014-01-06 05:39:38 +0100

    Internationalisierung wird für die Hauptoberfläche eingeführt. sample.fxml heißt jetzt main_content.fxml

--- src/com/github/odinasen/gui/DurakApplication.java
+++ src/com/github/odinasen/gui/DurakApplication.java
@@ -6,12 +6,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class DurakApplication extends Application {
 
-    @Override
+  private static final String BUNDLE_NAME = "com.github.odinasen.i18n.gui";
+  private static final String TITLE = "Hello World";
+  private static final String MAIN_FXML = "main_content.fxml";
+
+  @Override
     public void start(Stage primaryStage) throws Exception{
-      Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-      primaryStage.setTitle("Hello World");
+      ResourceBundle bundle = ResourceBundle.getBundle(BUNDLE_NAME, Locale.getDefault());
+      Parent root = FXMLLoader.load(
+          getClass().getResource(MAIN_FXML), bundle);
+      primaryStage.setTitle(TITLE);
       primaryStage.setScene(new Scene(root, 300, 275));
       primaryStage.show();
     }
