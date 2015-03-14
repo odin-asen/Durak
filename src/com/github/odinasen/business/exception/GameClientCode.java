4766d10da61659acab742c3e92ead86fa0ce1ae1
odin-asen <geroge.forman@freenet.de>
2015-03-14 01:22:41 +0100

    Das Grundgeruest fuer Error-Handling wird erstellt

---
+++ src/com/github/odinasen/business/exception/GameClientCode.java
@@ -0,0 +1,22 @@
+package com.github.odinasen.business.exception;
+
+/**
+ * <p/>
+ * Author: Timm Herrmann<br/>
+ * Date: 18.10.14
+ */
+public enum GameClientCode implements ErrorCode {
+  SERVER_NOT_FOUND(401),
+  SERVICE_NOT_FOUND(402);
+
+  private int errorNumber;
+
+  private GameClientCode(int errorNumber) {
+    this.errorNumber = errorNumber;
+  }
+
+  @Override
+  public int getNumber() {
+    return errorNumber;
+  }
+}
