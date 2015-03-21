07ec7a2e37e3c0183ceb00bd69ca1f21ca567003
odin-asen <geroge.forman@freenet.de>
2015-03-21 23:45:48 +0100

    Umstellung des Projekts auf Maven

--- src/com/github/odinasen/business/card/CardSuit.java
+++
@@ -1,9 +0,0 @@
-package com.github.odinasen.business.card;
-
-/**
- * <p/>
- * Author: Timm Herrmann<br/>
- * Date: 06.01.14
- */
-public enum CardSuit {
-}
--- src/com/github/odinasen/business/game/PlayerStatus.java
+++
@@ -1,9 +0,0 @@
-package com.github.odinasen.business.game;
-
-/**
- * <p/>
- * Author: Timm Herrmann<br/>
- * Date: 06.01.14
- */
-public enum PlayerStatus {
-}
